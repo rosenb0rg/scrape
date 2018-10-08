c0e1090eb33b4ceb8cbff568232432453548132f
rosenb0rg <rosenberg.thomas@gmail.com>
2018-10-08 23:11:34 +0200

    cleanup

--- craig.py
+++ craig.py
@@ -5,82 +5,78 @@ import urllib3
 import requests
 import random
 import os
+from PIL import Image
+
 
 urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
 
+
 def main():
-    #get_db()
-    get_images()
-
-
-def get_images():
-    db = TinyDB('./db.json')
-    for i in db.all():
-        for key in i:
-            url = key
-            time_stamp, city_name, descr = i[key]
-
-            print ('\nurl: ', url,
-                '\ntime: ', time_stamp,
-                '\ncity: ', city_name,
-                '\ndescription: ', descr)
-
-            out_dir = os.path.join('out', city_name.replace(' ', ''))
-            if not os.path.isdir(out_dir):
-                os.makedirs(out_dir)
-
-            img_name = url.split('/')[-1]
-
-            f = open('./%s/%s' % (out_dir, img_name),'wb')
-            f.write(requests.get(url).content)
-            f.close()
+    get_db() 
 
 
 def get_db():
+    # create a database for the image information
     global db
     db = TinyDB('./db.json')
 
+    # get a list of all the cities in the US craigslist covers
     address_dict = get_cities()
     cities = list(address_dict.items())
 
-    for city in cities:
+    # for each city find the free postings, extract the 300x300 image
+    # and write the image information to the database
+    for city in cities[34:]:
+        print (city)
         url = city[1] + 'd/free-stuff/search/zip'
         global city_name
-        city_name = city[0].replace(' ','')
-        city_name = city_name.replace('/','')
+        city_name = city[0].replace(' ', '')
+        city_name = city_name.replace('/', '')
 
-        counter = 0
         while url:
             print ("Web Page: ", url)
-            soup, counter = soup_process(url, counter)
+            soup = soup_process(url)
             nextlink = soup.find("link", rel="next")
 
             url = False
             if (nextlink):
                 url = nextlink['href']
 
-def soup_process(url, counter):
+
+def soup_process(url):
     soup = make_soup(url)
     results = soup.find_all("li", class_="result-row")
     for i, result in enumerate(results):
         try:
             img_url = clean_pic(result.a['data-ids'])
+            img_name = img_url.split('/')[-1]
             time_stamp = result.p.time['datetime']
             descr = result.p.a.string.strip()
-            counter = counter + 1
-            img_info = {img_url : [time_stamp, city_name, descr]}
+
+            img_info = {img_url: [time_stamp, city_name, descr]}
+
+            out_dir = os.path.join('.','out', city_name)
+            if not os.path.isdir(out_dir):
+                os.makedirs(out_dir)
+
+            out_path = os.path.join(out_dir, img_name)
+            img = Image.open(requests.get(img_url, stream = True).raw)
+            img.save(out_path)
+
+            # print ('\nimage: ', img_url, '\ncity: ', city_name)
             db.insert(img_info)
 
-        except (AttributeError, KeyError) as ex:
+        except (AttributeError, KeyError, OSError) as ex:
+            print (ex)
             pass
 
-    return soup, counter
+    return soup
 
 
 def make_soup(url):
     http = urllib3.PoolManager()
     r = http.request("GET", url)
-    return BeautifulSoup(r.data,'lxml')
+    return BeautifulSoup(r.data, 'lxml')
 
 
 def get_cities():
@@ -91,7 +87,7 @@ def get_cities():
     address_dict = {}
     for result in results:
         if result.text:
-            address = {result.contents[0]:result['href']}
+            address = {result.contents[0]: result['href']}
             address_dict.update(address)
 
     return address_dict
@@ -100,8 +96,9 @@ def get_cities():
 def clean_pic(ids):
     idlist = ids.split(",")
     first = idlist[0]
-    code = first.replace("1:","")
-    #return "https://images.craigslist.org/%s_1200x900.jpg" % code
+    code = first.replace("1:", "")
+    # return "https://images.craigslist.org/%s_1200x900.jpg" % code
     return "https://images.craigslist.org/%s_300x300.jpg" % code
+
 
 main()
